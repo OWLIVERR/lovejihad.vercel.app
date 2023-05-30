cba9b134b17f98718410c956348a0159f3ef936b
Pranshu Patel <pranshu05patel@gmail.com>
2023-05-30 20:16:40 +0530

    edit: edited video link

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,10 +8,7 @@ const Card = ({ name, date, content, videoUrl, whoIsJihadi }) => {
       <div className="content">
         {content}
         <div className="video">
-          <video width="100%" controls>
-            <source src={videoUrl} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+        <iframe width="640" height="360" frameborder="0" src={videoUrl} allowfullscreen ></iframe>
         </div>
         <h3>Who is Jihadi?</h3>
         {whoIsJihadi}
