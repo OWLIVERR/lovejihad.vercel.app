bce1c11fc243c492c3db9e0450fa99302bc8b5ca
Pranshu Patel <pranshu05patel@gmail.com>
2023-05-30 21:41:39 +0530

    Feat: made container responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const Card = ({ name, date, content, videoUrl, whoIsJihadi }) => {
       <div className="content">
         {content}
         <div className="video">
-          <iframe frameborder="0" src={videoUrl}></iframe>
+          <iframe frameBorder="0" src={videoUrl}></iframe>
         </div>
         <h3>Who is Jihadi?</h3>
         {whoIsJihadi}
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import cardData from "../Utils/victims.json";
+
 const Container = () => {
+  const [visibleCards, setVisibleCards] = useState(5);
+
+  const loadMoreCards = () => {
+    setVisibleCards((prevVisibleCards) => prevVisibleCards + 5);
+  };
+
+  const showPreviousCards = () => {
+    setVisibleCards((prevVisibleCards) => prevVisibleCards - 5);
+  };
+
+  const isFirstSet = visibleCards <= 5;
+  const isLastSet = visibleCards >= cardData.length;
+
   return (
     <div className="container">
-      {cardData.map((card, index) => (
+      {!isFirstSet && (
+        <button onClick={showPreviousCards}>Load Previous</button>
+      )}
+      {cardData.slice(visibleCards - 5, visibleCards).map((card, index) => (
         <Card
           key={index}
           name={card.name}
@@ -14,6 +31,7 @@ const Container = () => {
           whoIsJihadi={card.whoIsJihadi}
         />
       ))}
+      {!isLastSet && <button onClick={loadMoreCards}>Load More</button>}
     </div>
   );
 };
